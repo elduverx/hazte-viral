64ea53312f15f5bbba6d3d28840fb3740de2e44f
Duverney muriel <duverwild@gmail.com>
2025-12-08 15:10:09 +0100

    Initial Commit

---
+++ goviiral/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  goviiral
+//
+//  Created by duverney muriel on 8/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ goviiral/goviiralApp.swift
@@ -0,0 +1,17 @@
+//
+//  goviiralApp.swift
+//  goviiral
+//
+//  Created by duverney muriel on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct goviiralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
